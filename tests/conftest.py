8f39775a88d56ca4204f78fe4020c9efd1b0a8d5
mferrera <matt.ferrera@gmail.com>
2025-04-10 07:13:49 +0200

    ENH: Add 'masterdata' and 'smda' fields to config

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ import pytest
 from fmu.settings._fmu_dir import FMUDirectory
 from fmu.settings._init import init_fmu_directory
 from fmu.settings._version import __version__
-from fmu.settings.resources.config import Config
+from fmu.settings.models.config import Config
 
 
 @pytest.fixture
@@ -26,6 +26,57 @@ def config_dict(unix_epoch_utc: datetime) -> dict[str, Any]:
         "version": __version__,
         "created_at": unix_epoch_utc,
         "created_by": "user",
+        "masterdata": {
+            "smda": None,
+        },
+    }
+
+
+@pytest.fixture
+def masterdata_dict() -> dict[str, Any]:
+    """Example masterdata from SMDA."""
+    return {
+        "smda": {
+            "country": [
+                {
+                    "identifier": "Norway",
+                    "uuid": "ad214d85-8a1d-19da-e053-c918a4889309",
+                }
+            ],
+            "discovery": [
+                {
+                    "short_identifier": "DROGON",
+                    "uuid": "ad214d85-8a1d-19da-e053-c918a4889309",
+                }
+            ],
+            "field": [
+                {
+                    "identifier": "DROGON",
+                    "uuid": "ad214d85-8a1d-19da-e053-c918a4889309",
+                }
+            ],
+            "coordinate_system": {
+                "identifier": "ST_WGS84_UTM37N_P32637",
+                "uuid": "ad214d85-dac7-19da-e053-c918a4889309",
+            },
+            "stratigraphic_column": {
+                "identifier": "DROGON_HAS_NO_STRATCOLUMN",
+                "uuid": "ad214d85-8a1d-19da-e053-c918a4889309",
+            },
+        }
+    }
+
+
+@pytest.fixture
+def config_dict_with_masterdata(
+    unix_epoch_utc: datetime, masterdata_dict: dict[str, Any]
+) -> dict[str, Any]:
+    """A dictionary representing a .fmu config."""
+    return {
+        "version": __version__,
+        "created_at": unix_epoch_utc,
+        "created_by": "user",
+        "masterdata": masterdata_dict,
     }
 
 
@@ -33,6 +84,12 @@ def config_dict(unix_epoch_utc: datetime) -> dict[str, Any]:
 def config_model(config_dict: dict[str, Any]) -> Config:
     """A Config model representing a .fmu config file."""
     return Config.model_validate(config_dict)
+
+
+@pytest.fixture
+def config_model_with_masterdata(config_dict_with_masterdata: dict[str, Any]) -> Config:
+    """A Config model representing a .fmu config file."""
+    return Config.model_validate(config_dict_with_masterdata)
 
 
 @pytest.fixture
