74a00bce3d1e6ea2fe4a0d5aef06fb5eea3fede7
nourinmohd <nourin_mohd97@outlook.com>
2025-09-24 12:49:34 +0200

    DOC: Update doc string

--- src/fmu/settings/_resources/pydantic_resource_manager.py
+++ src/fmu/settings/_resources/pydantic_resource_manager.py
@@ -58,9 +58,8 @@ class PydanticResourceManager(Generic[T]):
             Validated Pydantic model
 
         Raises:
-            FileNotFoundError: If the resource file doesn't exist
-            ValidationError: If the data doesn't match the model schema
-        """
+            ValueError: If the resource file doesn't exist or if the data doesn't match the model schema
+        """  # noqa: E501
         if self._cache is None or force:
             if not self.exists:
                 raise FileNotFoundError(
